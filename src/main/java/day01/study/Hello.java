e2ff1075309d86989abec5069490462b3679ece3
18668495986 <601743940@qq.com>
2019-12-21 11:27:24 +0800

    I ok ?

---
+++ src/main/java/day01/study/Hello.java
@@ -0,0 +1,16 @@
+package day01.study;
+
+/**
+ * Description：xxx源程序<br/>
+ * Copyright (c) ，2019 ， xu <br/>
+ * This program is protected by copyright laws. <br/>
+ * Date： 2019年12月21日
+ *
+ * @author 徐威
+ * @version : 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello World!!");
+    }
+}
